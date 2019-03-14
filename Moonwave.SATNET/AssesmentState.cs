19f759b9b2d0d0f4732b17e8b176c29174c50827
Shane Johnson <36738000+CARVERD12@users.noreply.github.com>
2019-03-14 17:29:06 -0400

    Add project files.

---
+++ Moonwave.SATNET/AssesmentState.cs
@@ -0,0 +1,36 @@
+﻿// (c) 2019 Moonwave Interactive. All rights reserved under MIT License.
+
+namespace Moonwave.SATNET
+{
+    /// <summary>
+    /// Specifies constants that define assesment states.
+    /// </summary>
+    public enum AssesmentState
+    {
+        /// <summary>
+        /// The assesment data is valid for the current computer configuration.
+        /// </summary>
+        Valid,
+
+        /// <summary>
+        /// The assesmentdata is not valid.
+        /// </summary>
+        Invalid,
+
+        /// <summary>
+        /// The assesment data does not match the current computer configuration.
+        /// </summary>
+        Incoherent,
+
+        /// <summary>
+        /// The assesment data is not available because a formal WinSAT assesment
+        /// has not been run on this computer.
+        /// </summary>
+        Unavailable,
+
+        /// <summary>
+        /// The state of the assesment is unknown.
+        /// </summary>
+        Unknown
+    }
+}
